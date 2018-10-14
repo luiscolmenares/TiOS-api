989ae5993b2103719c8cc6235448ba5abd098460
Luis Colmenares <luiscolmenares@gmail.com>
2018-10-14 16:16:18 -0400

    kernel schedule events fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $noderedurl = "https://node-red.tiosplatform.com:1080";
+        
+
         $schedule->call(function () {
             $now =  \Carbon\Carbon::now()->format('Y-m-d H:i');
             $now_timestamp =  strtotime($now);
@@ -45,6 +46,7 @@ class Kernel extends ConsoleKernel
                 ])
             ->get();
             foreach ($events as $activeevent) {
+                $noderedurl = "https://node-red.tiosplatform.com:1080";
                 /* Check what action is needed
                  * send-email
                  * sms-message
@@ -89,7 +91,7 @@ class Kernel extends ConsoleKernel
                     //     # code...
                      $this->disableEvent($activeevent->id);
                     // $url_off = $action[0]['broker'].'/thingstatus?topic='.$action[0]['topic'].'&value=OFF';
-                     $url_off = $noderedurl.'/thingstatus?topic='.$action['topic'].'&value=OFF';
+                     $url_off = $noderedurl.'/thingstatus?topic='.$action['topic'].'/control&value=OFF';
                      $client = new Client(); //GuzzleHttp\Client
                      $response = $client->get($url_off);
                     // $response = $client->get('http://ec2-54-208-184-235.compute-1.amazonaws.com:1880/thingstatus?topic=room/b1&value=0');
@@ -97,7 +99,8 @@ class Kernel extends ConsoleKernel
                      case 'turn-on':
                         # code...
                       // $url_on = $action[0]['broker'].'/thingstatus?topic='.$action[0]['topic'].'&value=ON';
-                      $url_on = $noderedurl.'/thingstatus?topic='.$action['topic'].'&value=ON';
+                      $url_on = $noderedurl.'/thingstatus?topic='.$action['topic'].'/control&value=ON';
+                      echo $url_on;
                      //$url_on = 'http://ec2-54-208-184-235.compute-1.amazonaws.com:1880/thingstatus?topic=room/b1&value=1';
                       $this->disableEvent($activeevent->id);
                     $client = new Client(); //GuzzleHttp\Cliente
@@ -106,6 +109,9 @@ class Kernel extends ConsoleKernel
          //Twilio::message('+17862032077',$url_on);
                         break;
                     case 'new-value':
+                     $url_on = $noderedurl.'/thingstatus?topic='.$action['topic'].'/control&value='.$action['new-value'];
+                     $client = new Client(); //GuzzleHttp\Cliente
+                     $response = $client->get($url_on);
                     $this->disableEvent($activeevent->id);
                         # code...
                         break;
